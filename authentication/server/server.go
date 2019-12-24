c2afdc14388a334e9db0bb04443cd9020e3e89d2
krishnaeverestengineering <krishnamurthy.t@everest.engineering>
2019-12-24 14:42:27 +0530

    changed REST Lib Gin to Gorilla Mux

--- authentication/server/server.go
+++ authentication/server/server.go
@@ -4,28 +4,27 @@ import (
 	"Filebox-Micro/authentication/endpoint"
 	"Filebox-Micro/authentication/transport"
 	"context"
+	"net/http"
 
 	kithttp "github.com/go-kit/kit/transport/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
 //NewHTTPServer returns router configuration
-func NewHTTPServer(ctx context.Context, endpoints endpoint.Endpoints) *gin.Engine {
-	r := gin.Default()
-	r.Use(commonMiddleware())
-	r.GET("/ServiceLogin", func(c *gin.Context) {
-		kithttp.NewServer(
-			endpoints.CreateUser,
-			transport.DecodeRequest,
-			transport.EncodeResponse,
-		).ServeHTTP(c.Writer, c.Request)
-	})
+func NewHTTPServer(ctx context.Context, endpoints endpoint.Endpoints) http.Handler {
+	r := mux.NewRouter()
+	r.Use(commonMiddleware)
+	r.Methods("GET").Path("/ServiceLogin").Handler(kithttp.NewServer(
+		endpoints.CreateUser,
+		transport.DecodeRequest,
+		transport.EncodeResponse,
+	))
 	return r
 }
 
-func commonMiddleware() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Add("Context-Type", "application/json")
-	}
+func commonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
